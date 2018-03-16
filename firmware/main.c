ba61e6dda95e4e6ac984e3549b4065469d2fc6f8
akmorrison <akmorris@edu.uwaterloo.ca>
2018-03-16 09:46:53 -0400

    Initialize firmware project

---
+++ firmware/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: aaronmorrison
+ *
+ * Created on March 16, 2018, 9:40 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
